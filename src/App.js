6d4e3bb684a5c0344e76c7f1775c3387f7d2f519
Chris Mems <christopher.mems@.com>
2023-06-28 19:08:33 -0500

    create the starter componets and pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router";
+import LandingPage from "./pages/LandingPage";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import StudyPage from "./pages/StudyPage";
+import EditCard from "./pages/EditCard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/' element={<LandingPage />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/signup' element={<Signup />} />
+      <Route path='/study-page' element={<StudyPage />} />
+      <Route path='/edit-card' element={<EditCard />} />
+    </Routes>
   );
 }
 
