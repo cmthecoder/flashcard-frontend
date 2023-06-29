5f6f9169000f7902c515d1fc9c1abd25188bb951
Chris Mems <christopher.mems@.com>
2023-06-28 19:38:12 -0500

    set up sign up and login form and style the forms

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,9 +1,99 @@
-import React from 'react'
+import { useState } from "react";
 
 const Signup = () => {
-  return (
-    <div>Signup</div>
-  )
+    const [formData, setFormData] = useState({
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+    });
+  
+    const handleChange = (e) => {
+      setFormData((prevFormData) => ({
+        ...prevFormData,
+        [e.target.name]: e.target.value,
+      }));
+    };
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+
+      console.log(formData);
+      
+      setFormData({
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+      });
+    };
+  
+    return (
+      <div className="flex items-center justify-center h-screen">
+
+      <form className="max-w-sm mx-auto px-4" onSubmit={handleSubmit}>
+        <div className="mb-4">
+          <label
+            htmlFor="name"
+            className="block mb-2 text-sm font-medium text-gray-700 text-center"
+          >
+            Name:
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.firstName}
+            onChange={handleChange}
+            className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="email"
+            className="block mb-2 text-sm font-medium text-gray-700 text-center"
+          >
+            Email Address:
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="password"
+            className="block mb-2 text-sm font-medium text-gray-700 text-center"
+          >
+            Password:
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+            required
+          />
+        </div>
+        <div className="flex items-center justify-center">
+          <button
+            type="submit"
+            className="w-full px-4 py-2 text-sm font-medium text-white bg-indigo-600 rounded-md hover:bg-indigo-500 focus:outline-none focus:bg-indigo-500"
+          >
+            Sign Up
+          </button>
+        </div>
+      </form>
+      </div>
+    );
 }
 
 export default Signup
